f69b70054a8d37c9d628e55846c4156f51d792e9
macournoyer <macournoyer@gmail.com>
2014-03-26 13:37:53 -0400

    Use older parser API.

--- http_parser.js
+++ http_parser.js
@@ -1,20 +1,19 @@
-// Extend Node internal parser to take care of data buffering and have a nicer API.
-// See parser_demo.js for sample usage.
-
+// Extend Node internal parser to take care of data buffering.
 var HTTPParser = process.binding('http_parser').HTTPParser,
     events = require('events')
 
 // Make HTTPParser inherit from EventEmitter without changing it's prototype.
 HTTPParser.prototype.__proto__ = events.EventEmitter.prototype
 
-// Constants used by the parser for callbacks.
-// Taken from Node internals.
-var kOnHeaders = HTTPParser.kOnHeaders | 0
-var kOnHeadersComplete = HTTPParser.kOnHeadersComplete | 0
-var kOnBody = HTTPParser.kOnBody | 0
-var kOnMessageComplete = HTTPParser.kOnMessageComplete | 0
 
-// Creates and return an new parser.
+HTTPParser.prototype.parse = function(data) {
+  var buffer = this.buffer = this.buffer || "";
+  var start = buffer.length;
+  buffer += data;
+  
+  this.execute(new Buffer(buffer), start, data.length);
+};
+
 exports.createParser = function() {
   var parser = new HTTPParser(HTTPParser.REQUEST),
       info
@@ -22,25 +21,14 @@ exports.createParser = function() {
   events.EventEmitter.call(parser)
 
   // Store headers
-  parser[kOnHeadersComplete] = function(headers) {
+  parser.onHeadersComplete = function(headers) {
     info = headers
     info.method = HTTPParser.methods[info.method]
   }
 
-  parser[kOnMessageComplete] = function() {
+  parser.onMessageComplete = function() {
     parser.emit('request', info)
   }
 
   return parser
-}
-
-// Feed a string to the parser.
-// `onComplete` will be called if this results in a complete HTTP request.
-HTTPParser.prototype.parse = function(data) {
-  // Buffer data
-  var buffer = this.buffer = this.buffer || ""
-  var start = buffer.length
-  buffer += data
-  
-  this.execute(new Buffer(buffer), start, data.length)
 }
